05184f8f152d55f179101105356285dc2b40f6a8
santosh <santoshpasi6566@gmail.com>
2023-07-25 18:13:06 +0530

    Upto mid of DOM Part2

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const taskContainer=document.querySelector('.task_container');
-const globalStorage=[];
-const newcard=(taskData)=>
-    `<div class="col-md-6 col-lg-4" id="${taskData.id}">
+let globalStorage=[];
+const newcard=({
+  id,
+  imageUrl,
+  taskTitle,
+  taskType,
+  taskDescription,
+})=> `<div class="col-md-6 col-lg-4" id="${id}">
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-1">
         <button type="button" class="btn btn-outline-info"><i class="fa-solid fa-pen-to-square"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-trash-can"></i></button>
+        <button type="button" class="btn btn-outline-danger" id="${id}" onclick="deleteCard.apply(this,arguments)"> <i class="fa-solid fa-trash-can"></i></button>
       </div>
 
-      <img src="${taskData.imageUrl}" class="card-img-top" alt="img">
+      <img src="${imageUrl}" class="card-img-top" alt="img">
 
       <div class="card-body">
-        <h5 class="card-title">${taskData.taskTitle}</h5>
-        <p class="card-text">${taskData.taskDescription}</p>
-        <span class="badge bg-primary">${taskData.taskType}</span>
+        <h5 class="card-title">${taskTitle}</h5>
+        <p class="card-text">${taskDescription}</p>
+        <span class="badge bg-primary">${taskType}</span>
       </div>
       <div class="card-footer text-body-secondary d-flex justify-content-end">
         <button type="button" class="btn btn-outline-primary">Open Task</button>
@@ -51,4 +56,18 @@ const saveChanges=()=>{
     globalStorage.push(taskData);
     localStorage.setItem("Tasky",JSON.stringify({cards:globalStorage}));    
     
-}
+};
+
+const deleteCard = (Event) =>{
+  event = window.Event;
+  const targetID = Event.target.id;
+
+  const newUpdatedArray=globalStorage.filter(
+    (cardObject) => cardObject.id !== targetID
+  );
+
+  newUpdatedArray.map((cardObject)=>{ const createnewcard = newcard(cardObject);
+    taskContainer.insertAdjacentHTML('beforeend', createnewcard);
+  })
+  globalStorage=newUpdatedArray;
+};
